27f6bc67cc3265f548aa29d91072eaeea8794cd9
Ilya Liverts <elibrch@gmail.com>
2014-07-05 12:43:56 +0300

    Initial commit

---
+++ test.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <title>Shirts 4 Mike</title>
+<head>
+    <body>
+        <h1>Shirts 4 Mike</h1>
+        <p>&copy;Today is <?php echo date('l') ?>, <?php echo date('M') ?> <?php echo date('d') ?>, <?php echo date('Y') ?> </p>
+        <p>&copy;<?php echo date('Y'); ?> Shirts 4 Mike</p>
+    </body>
+</head>
+</html>
